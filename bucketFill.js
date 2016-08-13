b8941a00a2706b06b37f4a18667f088d5ccf4f09
Prashant Singh Pawar <prashantpawar@gmail.com>
2016-08-13 18:43:33 -0400

    -bucket fill works

--- bucketFill.js
+++ bucketFill.js
@@ -1,6 +1,24 @@
 "use strict";
 
-module.exports = function bucketFill(commandElements, screenBuffer) {
+module.exports = function bucketFill(commandElements, inScreenBuffer) {
   console.log('bucketFill', commandElements);
-  return screenBuffer;
+
+  var outScreenBuffer = inScreenBuffer.slice(); //to clone the array
+
+  var x = parseInt(commandElements[1], 10);
+  var y = parseInt(commandElements[2], 10);
+  var c = commandElements[3];
+
+  //Our screenBuffer is stored as Y,X
+  if(outScreenBuffer[y][x] !== ' ') {
+      return outScreenBuffer;
+  }
+  outScreenBuffer[y][x] = c;
+
+  bucketFill(['B', x+1, y, c], outScreenBuffer);
+  bucketFill(['B', x, y+1, c], outScreenBuffer);
+  bucketFill(['B', x-1, y, c], outScreenBuffer);
+  bucketFill(['B', x, y-1, c], outScreenBuffer);
+
+  return outScreenBuffer;
 };
